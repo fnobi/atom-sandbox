d980023958f7e2a5733af44c090dc235ac8ac1a1
nobii <fujisawa-shin@kayac.com>
2018-01-07 14:59:38 +0900

    first commit. (suddun-death作成)

---
+++ spec/atom-sandbox-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSandboxView from '../lib/atom-sandbox-view';
+
+describe('AtomSandboxView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
